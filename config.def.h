a0a07be5d8ea2a1ba2791347c091ce92ff86b6b9
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2020-06-05 23:37:59 +0700

    change: config.def.h

--- config.def.h
+++ config.def.h
@@ -60,16 +60,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-l", "30", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
-static const char *usKeyboard[] = { "setxkbmap", "us", NULL};
-static const char *dvKeyboard[] = { "setxkbmap", "dvorak", NULL};
+static const char *switchKeyboard[] = { "keyboard-toggle", NULL};
+static const char *swapescape[] = { "swapesc", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_Tab,    spawn,          {.v = dvKeyboard } },
-	{ MODKEY|ControlMask,           XK_Tab,    spawn,          {.v = usKeyboard } },
-	{ MODKEY,                       XK_Caps_Lock,      spawn,          {.v = lockcmd} },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd} },
+	{ MODKEY|ShiftMask,             XK_Tab,    spawn,          {.v = switchKeyboard } },
+	{ MODKEY|ControlMask,           XK_Tab,    spawn,          {.v = swapescape } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
